a5a28a0114f3aba4a50c3ec6e23f9b5a1bc71fa5
phan tien <tien.pv181277@gmail.com>
2024-03-17 23:32:04 +0700

    C

--- src/main/java/com/integrationninjas/springbootexample/SpringbootExampleApplication.java
+++ src/main/java/com/integrationninjas/springbootexample/SpringbootExampleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootExampleApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello World");
 		SpringApplication.run(SpringbootExampleApplication.class, args);
 	}
 
