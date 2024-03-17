e6a97a545fb650b70f6168f77445ce0f86b1ab11
phan tien <tien.pv181277@gmail.com>
2024-03-17 23:47:54 +0700

    C7

--- src/main/java/com/integrationninjas/springbootexample/SpringbootExampleApplication.java
+++ src/main/java/com/integrationninjas/springbootexample/SpringbootExampleApplication.java
@@ -2,12 +2,20 @@ package com.integrationninjas.springbootexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/hello")
 public class SpringbootExampleApplication {
 
+	@GetMapping
+	public String getHello(){
+		return "Hello World";
+	}
 	public static void main(String[] args) {
-		System.out.println("Hello World");
 		SpringApplication.run(SpringbootExampleApplication.class, args);
 	}
 
